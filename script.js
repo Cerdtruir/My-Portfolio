3677d93bd05ff4c1eb0529f1fe3b55bf87c48726
Cerdtruir <ghoornabeel@gmail.com>
2022-02-14 14:44:45 +0200

    add portfolio object for 4 portfolios

--- script.js
+++ script.js
@@ -19,3 +19,66 @@ document.querySelector('.header-menu-li-mobile-second').onclick = function () {
 document.querySelector('.header-menu-li-mobile-third').onclick = function () {
   mobileMenu.style.display = 'none';
 };
+
+const portfolioObjects = {
+  portfolioObject1: {
+    heading: 'Tonic',
+    subheading:
+      '<li class="portfolio-list-first">Canopy</li> <li class="portfolio-list-dot"></li> <li class="portfolio-list-secondary">Back End Dev</li> <li class="portfolio-list-dot"></li> <li class="portfolio-list-secondary">2022</li>',
+    image: 'images/Portfolio-screenshot1.png',
+    shortDescription:
+      'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    description:
+      'A daily selection of privately personalized readsA daily selection of privately personalized reads A daily selection of privately personalized reads A daily selection of privately personalized reads A daily selection of privately personalized reads of privately personalized reads; no accounts or sign-ups required. ',
+    languages:
+      '<li class="language-text">html</li> <li class="language-text">css</li> <li class="language-text">javascript</li>',
+    seeProjectButton: 'See Project',
+    source: 'github.com',
+    liveVersion: 'github.com',
+  },
+  portfolioObject2: {
+    heading: 'Multi-Post Stories',
+    subheading:
+      '<li class="portfolio-list-first">Canopy</li> <li class="portfolio-list-dot"></li> <li class="portfolio-list-secondary">Back End Dev</li> <li class="portfolio-list-dot"></li> <li class="portfolio-list-secondary">2022</li>',
+    image: 'images/Portfolio-screenshot2.png',
+    shortDescription:
+      'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    description:
+      'A daily selection of privately personalized readsA daily selection of privately personalized reads A daily selection of privately personalized reads A daily selection of privately personalized reads A daily selection of privately personalized reads of privately personalized reads; no accounts or sign-ups required. ',
+    languages:
+      '<li class="language-text">html</li> <li class="language-text">css</li> <li class="language-text">javascript</li>',
+    seeProjectButton: 'See Project',
+    source: 'github.com',
+    liveVersion: 'github.com',
+  },
+  portfolioObject3: {
+    heading: 'Talk Type All',
+    subheading:
+      '<li class="portfolio-list-first">Canopy</li> <li class="portfolio-list-dot"></li> <li class="portfolio-list-secondary">Back End Dev</li> <li class="portfolio-list-dot"></li> <li class="portfolio-list-secondary">2022</li>',
+    image: 'images/Portfolio-screenshot3.png',
+    shortDescription:
+      'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    description:
+      'A daily selection of privately personalized readsA daily selection of privately personalized reads A daily selection of privately personalized reads A daily selection of privately personalized reads A daily selection of privately personalized reads of privately personalized reads; no accounts or sign-ups required. ',
+    languages:
+      '<li class="language-text">html</li> <li class="language-text">css</li> <li class="language-text">javascript</li>',
+    seeProjectButton: 'See Project',
+    source: 'github.com',
+    liveVersion: 'github.com',
+  },
+  portfolioObject4: {
+    heading: 'Say Much Self',
+    subheading:
+      '<li class="portfolio-list-first">Canopy</li> <li class="portfolio-list-dot"></li> <li class="portfolio-list-secondary">Back End Dev</li> <li class="portfolio-list-dot"></li> <li class="portfolio-list-secondary">2022</li>',
+    image: 'images/Portfolio-screenshot4.png',
+    shortDescription:
+      'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    description:
+      'A daily selection of privately personalized readsA daily selection of privately personalized reads A daily selection of privately personalized reads A daily selection of privately personalized reads A daily selection of privately personalized reads of privately personalized reads; no accounts or sign-ups required. ',
+    languages:
+      '<li class="language-text">html</li> <li class="language-text">css</li> <li class="language-text">javascript</li>',
+    seeProjectButton: 'See Project',
+    source: 'github.com',
+    liveVersion: 'github.com',
+  },
+};
