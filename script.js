7d00b014ee1fe30c1815813aa82db0f6eca5849b
Cerdtruir <ghoornabeel@gmail.com>
2022-03-02 14:14:41 +0200

    Reduce js hide menu functions size

--- script.js
+++ script.js
@@ -4,18 +4,8 @@ document.getElementById('hamburger').onclick = function () {
   mobileMenu.style.display = 'block';
 };
 
-document.querySelector('.close').onclick = function () {
-  mobileMenu.style.display = 'none';
-};
-
-document.querySelector('.header-menu-li-mobile-first').onclick = function () {
-  mobileMenu.style.display = 'none';
-};
-
-document.querySelector('.header-menu-li-mobile-second').onclick = function () {
-  mobileMenu.style.display = 'none';
-};
-
-document.querySelector('.header-menu-li-mobile-third').onclick = function () {
-  mobileMenu.style.display = 'none';
-};
+document.querySelectorAll('.hide-menu').forEach((element) => {
+  element.onclick = function () {
+    mobileMenu.style.display = 'none';
+  };
+});
